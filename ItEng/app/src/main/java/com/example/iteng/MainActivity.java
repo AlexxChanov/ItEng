06c1341800dc3118eca1c4728c80235233e9c4fe
AlexxChanov <52286366+AlexxChanov@users.noreply.github.com>
2020-01-11 15:07:06 +0300

    add project

---
+++ ItEng/app/src/main/java/com/example/iteng/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.iteng;
+
+import android.os.Build;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.annotation.RequiresApi;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar=(Toolbar)findViewById(R.id.myToolbar);
+        tabLayout=(TabLayout)findViewById(R.id.tablayout);
+        viewPager=(ViewPager) findViewById(R.id.view_pager);
+
+
+        setSupportActionBar(toolbar);
+        setupViewPager(viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new WordsFragment(),"Слова");
+        viewPagerAdapter.addFragment(new ArticlesFragment(),"Статьи");
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+    }
+
+}
